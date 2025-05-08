cad2d12f3a76b960b76e7401933fe472e338faa7
dianabello <leidyaparicio2703@gmail.com>
2025-05-08 15:14:45 -0500

    eres mayor o menor de edad

---
+++ MayorMenorEdad.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MayorMenorEdad {
+    public static void main(String[] args) throws Exception {
+        Scanner entradaUsuario = new Scanner(System.in);
+
+        int edadUsuario;
+
+        System.out.println("Ingresa tu edad: ");
+        edadUsuario = entradaUsuario.nextInt();
+
+        if (edadUsuario >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
+
+        entradaUsuario.close();
+    }
+}
